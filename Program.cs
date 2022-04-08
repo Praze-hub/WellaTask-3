210cfefa2059ba2e1cff43c64e29fd2d643f92ac
Praze-hub <godspraiseokechukwu07@gmail.com>
2022-04-08 10:46:47 +0100

    Task 3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking fo prime number");
+            Console.Write("Enter prime number:");
+            int primeNumber = int.Parse(Console.ReadLine());
+            if(primeNumber == 17){
+                Console.WriteLine("Prime number");
+            }
+            if(primeNumber == 57){
+                Console.WriteLine("not prime number");
+            }
+            else{
+                Console.WriteLine("invalid input");
+
+            }
+
+      
+         
+        }
+    }
+}
